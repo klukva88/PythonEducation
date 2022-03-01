7bb600b46547b27820c6a99b2e91b8c12a4bb2ce
Nikolay <klukva182@gmail.com>
2022-03-01 22:02:55 +0600

    Lesson 2 - Domashka

---
+++ Lesson 3/practice Lesson 3.py
@@ -0,0 +1,17 @@
+# Задача 1
+i = 13
+while i>0 :
+    print(i, end=" ")
+    i= i-1
+print('\n')
+
+# Задача 2
+
+for i in range(1, 20, 2):
+    totalSum = i+i
+print(totalSum, end=" ")
+
+# Задача 4
+for i in range (37, 64):
+    if i%2 == 0:
+        print(i)
