4e16d6ac2ea3e69eea17e5e71e1ad779573ac5ab
Nikolay <klukva182@gmail.com>
2022-03-12 17:06:40 +0600

    Tuples Homework

--- Tuples/Tuples Practice.py
+++ Tuples/Tuples Practice.py
@@ -37,36 +37,7 @@ while index < len(myTuple):
     index += 1
 
 
-#Множества
 
-#Задача 1
-sampleSet = {"Yellow", "Orange", "Black"}
-sampleList = ["Blue", "Green", "Red"]
-
-sampleSet.update(sampleList)
-print(sampleSet)
-
-#Задача 2
-
-set1 = {10, 20, 30, 40, 50}
-set2 = {30, 40, 50, 60, 70}
-print(set1.intersection(set2))
-
-#Задача 3
-
-set1 = {10, 20, 30}
-set2 = {20, 40, 50}
-print(set1.difference(set2))
-
-#Задача 4
-newArray = sampleSet.union(set1)
-
-
-newArray.add(11)
-newArray.add('Vasya')
-
-newArray.update(['hh', 'ff', 'dd'], {'zz', 'rr'})
-print(newArray)
 
 
 
